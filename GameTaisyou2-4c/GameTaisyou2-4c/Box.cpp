bf0ed0085285ada50ff6c5d266fdc050cf65a626
初心者狩りのゾロ <106945336+nakadafumiya@users.noreply.github.com>
2023-02-09 09:31:50 +0900

    宝箱のプログラムの追加
    クラス作った

---
+++ GameTaisyou2-4c/GameTaisyou2-4c/Box.cpp
@@ -0,0 +1,17 @@
+#include "Box.h"
+#include "DxLib.h"
+
+Box::Box()
+{
+	
+}
+
+void Box::Update()
+{
+
+}
+
+void Box::Draw()const
+{
+
+}
---
+++ GameTaisyou2-4c/GameTaisyou2-4c/Box.h
@@ -0,0 +1,13 @@
+#pragma once
+class Box
+{
+private:
+	int DaggerImage;//’ZŒ•‚ÌƒCƒ[ƒW‰æ‘œ•Ï”
+
+
+public:
+
+	void Update();
+	void Draw() const;
+};
+
