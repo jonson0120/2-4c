646b31c9a14da66054951ed0eb6c8fcb74857110
erimaki1844 <106945196+erimaki1844@users.noreply.github.com>
2023-04-26 15:43:41 +0900

    チュートリアルスキップとタイトルのSE

--- GameTaisyou2-4c/GameTaisyou2-4c/TitleScene.cpp
+++ GameTaisyou2-4c/GameTaisyou2-4c/TitleScene.cpp
@@ -9,10 +9,16 @@ Title::Title() {
 	select = 0;
 	JoyPadY = 0;
 
+	Tutorial_flg = true;
+
 	select_image = LoadGraph("images/select.png");
+	mini_select_image = LoadGraph("images/select.png");
+	Tutorial_font = LoadGraph("images/Tutorial.png");
 	Title_font= LoadGraph("images/TITLE.png");
 	Start_font= LoadGraph("images/START.png");
 	Debug_font= LoadGraph("images/null.png");
+
+	click_sound = LoadSoundMem("sound/click.mp3");
 }
 
 AbstractScene* Title::Update() {
@@ -34,12 +40,24 @@ AbstractScene* Title::Update() {
 	if (select < 0)select = 1;
 	if (select > 1)select = 0;
 
-	if (PAD_INPUT::OnPressed(XINPUT_BUTTON_B) && interval >= 30) {
-		if (TITLE_MENU::START == Menu_Number)return new HowToMap();
-		if (TITLE_MENU::Debug == Menu_Number)return new TestMap();
-		interval = 0;
+	if (Tutorial_flg == false) {
+		if (PAD_INPUT::OnPressed(XINPUT_BUTTON_B) && interval >= 30) {
+			PlaySoundMem(click_sound, DX_PLAYTYPE_NORMAL);
+			if (TITLE_MENU::START == Menu_Number) Tutorial_flg = true;
+			if (TITLE_MENU::Debug == Menu_Number)return new TestMap();
+			interval = 0;
+		}
 	}
+	if (Tutorial_flg == true) {
+		if (PAD_INPUT::OnPressed(XINPUT_BUTTON_B) && interval >= 30) {
+			PlaySoundMem(click_sound, DX_PLAYTYPE_NORMAL);
+			if (TITLE_MENU::START == Menu_Number) return new GameMainScene(); //ゲームがスタートする
+			if (TITLE_MENU::Debug == Menu_Number)return new HowToMap(); //チュートリアルがスタートする
+			interval = 0;
+		}
 
+	}
+	
 	return this;
 }
 
@@ -49,10 +67,15 @@ void Title::Draw() const{
 	DrawFormatString(540, 360, 0xffffff, "START");
 	DrawFormatString(540, 460, 0xffffff, "Debug");*/
 	DrawGraph(380, 100, Title_font, TRUE);
-	DrawGraph(440, 360, Start_font, TRUE);
-	DrawGraph(440, 460, Debug_font, TRUE);
-	DrawGraph(400, 360 + select * 100, select_image, TRUE);
-
+	if (Tutorial_flg == false) {
+		DrawGraph(440, 360, Start_font, TRUE);
+		DrawGraph(440, 460, Debug_font, TRUE);
+		DrawGraph(400, 360 + select * 100, select_image, TRUE);
+	}
+	else {
+		DrawRotaGraph(630, 400, 2.0, 0, Tutorial_font, FALSE);
+		DrawRotaGraph(550, 430 + select * 32, 0.5, 0, mini_select_image, TRUE);
+	}
 	DrawFormatString(0, 500, 0xff0000, "%d", select);
 	DrawFormatString(0, 550, 0xff0000, "%d", interval);
 	DrawFormatString(0, 600, 0xff0000, "%d", JoyPadY);;
